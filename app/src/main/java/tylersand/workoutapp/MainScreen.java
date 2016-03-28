81a863652610e11c38c905183fd9ab828f74cc8e
DrSilicon <silicon.oxygen.tylerhedron@gmail.com>
2016-03-28 12:49:47 -0600

    Add app

---
+++ app/src/main/java/tylersand/workoutapp/MainScreen.java
@@ -0,0 +1,14 @@
+package tylersand.workoutapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+    //YOU CAN'T TRUST THE SYSTEM MAAAANNNN!!! I'M AN ADUUULLLLT!  YOU CAN'T BUY ME HOTDOG MAN
+}
