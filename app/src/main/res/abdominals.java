b5b13577f7bf13a1c060a03675968219024ac59a
DrSilicon <silicon.oxygen.tylerhedron@gmail.com>
2016-03-29 12:48:04 -0600

    Added abdominals and reach page

---
+++ app/src/main/res/abdominals.java
@@ -0,0 +1,13 @@
+import android.os.Bundle;
+
+import tylersand.workoutapp.R;
+
+/**
+ * Created by Tyler on 3/28/2016.
+ */
+public class abdominals {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
