fe24548837d28f5cad60baf80b9495e29509af87
Austin Emerick <awemerick117@gmail.com>
2016-03-31 13:12:05 -0600

    added onlciked for the times and workouts buttons on the main screen

--- app/src/main/java/tylersand/workoutapp/MainScreen.java
+++ app/src/main/java/tylersand/workoutapp/MainScreen.java
@@ -1,7 +1,10 @@
 package tylersand.workoutapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 //
 public class MainScreen extends AppCompatActivity {
 
@@ -11,4 +14,22 @@ public class MainScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_screen);
     }
     //YOU CAN'T TRUST THE SYSTEM MAAAANNNN!!! I'M AN ADUUULLLLT!  YOU CAN'T BUY ME HOTDOG MAN
+    //
+    public void openWorkoutsActivity(View view) {
+      // Do something in response to button
+
+        Intent intent = new Intent(this, workouts.class);
+
+        startActivity(intent);
+
+    }
+    public void openTimesActivity(View view) {
+        // Do something in response to button
+
+        Intent intent = new Intent(this, times.class);
+
+        startActivity(intent);
+
+    }
 }
+
