a83c21483f75f0d2e79bff8bc3ddbeee4f6edd0b
Austin Emerick <awemerick117@gmail.com>
2016-04-01 12:54:26 -0600

    added onclick to the buttons on the workouts.xml and the methods in the workouts.java

--- app/src/main/java/workouts/xml/workouts.java
+++ app/src/main/java/workouts/xml/workouts.java
@@ -1,5 +1,6 @@
 package workouts.xml;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import tylersand.workoutapp.MainScreen;
 import tylersand.workoutapp.R;
 
 public class workouts extends AppCompatActivity {
@@ -26,6 +28,94 @@ public class workouts extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+    public void goToMain(View view) {
+        // Do something in response to button
+
+        Intent intent = new Intent(this, MainScreen.class);
+
+        startActivity(intent);
+
+    }
+    public void goToAbdominals(View view) {
+        // Do something in response to button
+
+        Intent intent = new Intent(this, Abdominals.class);
+
+        startActivity(intent);
+
+    }
+
+    public void goToTriceps(View view) {
+        // Do something in response to button
+
+        Intent intent = new Intent(this, Triceps.class);
+
+        startActivity(intent);
+
+    }
+
+    public void goToBiceps(View view) {
+        // Do something in response to button
+
+        Intent intent = new Intent(this, Biceps.class);
+
+        startActivity(intent);
+
+    }
+
+    public void goToButts(View view) {
+        // Do something in response to button
+
+        Intent intent = new Intent(this, Gluteusmuscles.class);
+
+        startActivity(intent);
+
+    }
+
+    public void goToQuadriceps(View view) {
+        // Do something in response to button
+
+        Intent intent = new Intent(this, Quadriceps.class);
+
+        startActivity(intent);
+
+    }
+
+    public void goToShoulders(View view) {
+        // Do something in response to button
+
+        Intent intent = new Intent(this, Shoulders.class);
+
+        startActivity(intent);
+
+    }
+
+    public void goToPectorals(View view) {
+        // Do something in response to button
+
+        Intent intent = new Intent(this, Pectorals.class);
+
+        startActivity(intent);
+
+    }
+
+    public void goToCalves(View view) {
+        // Do something in response to button
+
+        Intent intent = new Intent(this, Calves.class);
+
+        startActivity(intent);
+
+    }
+
+    public void goToBack(View view) {
+        // Do something in response to button
+
+        Intent intent = new Intent(this, Back.class);
+
+        startActivity(intent);
+
     }
 
 }
